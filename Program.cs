627b9fd732636fed199763a2e2c41313edf82e3f
racheli-rozental <racheli74621@gmail.com>
2025-02-15 20:03:21 +0200

    server

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("http://localhost:3000")
+        builder => builder.WithOrigins("https://client-ggbs.onrender.com")
                           .AllowAnyHeader()
                           .AllowAnyMethod());
 });
